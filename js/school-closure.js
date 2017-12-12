cfb219d11133a3625c54e99b6f8a6b79c93b7b2e
Nick Freear <nfreear@yahoo.co.uk>
2017-12-12 19:28:11 +0000

    Travis semistandard fixes to Javascript; add `?s=XX` param support

--- js/school-closure.js
+++ js/school-closure.js
@@ -2,31 +2,39 @@
   © NDF, 12-dec-2017.
 */
 
-jQuery(function ($) {
+window.jQuery(function ($) {
   'use strict';
 
   var $widget = $('#school-closure');
   var $conf = $('div[ data-school-closure ]');
-  var config = $conf.data( 'schoolClosure' );
-  var url = config.url || './../index.json';
-  var tpl = '<a title="Last updated: %t" href="%u"><i>%s </i><b>%oc</b></a>';
+  var config = $conf.data('schoolClosure');
+  var query = window.location.search;
+  var m_search = query.match(/\?s=([\w%]+)/);
 
-  console.warn($conf, $conf.data( 'schoolClosure' ), config);
+  config.m_search = m_search;
+  config.name = config.name || m_search ? decodeURIComponent(m_search[ 1 ]) : null;
+  config.tpl = '<a title="Last updated: %t" href="%u"><i>%s </i><b>%oc</b></a>';
+  config.url = config.url || './../index.json';
 
-  $.getJSON(url).done(function (data) {
+  console.warn('Config:', config, $conf);
+  $widget.addClass('school-closure-js');
+
+  $.getJSON(config.url).done(function (data) {
     console.warn('JSON:', data);
 
     for (var idx in data.schools) {
       var school = data.schools[ idx ];
 
-      if (school.name == config.name) {
+      if (school.name === config.name) {
         console.warn('Match:', school);
 
-        var html = tpl.replace(/%u/, data.home_url).replace(/%s/, school.name)
-          .replace(/%oc/, school.status).replace(/%t/, data.build_time);
-        $widget.html(html).addClass('school-closure-js').addClass(school.status); //.attr({ title: 'Last update: ' + data.build_time });
-        break;
+        var html = config.tpl.replace(/%u/, data.home_url).replace(/%s/, school.name)
+            .replace(/%oc/, school.status).replace(/%t/, data.build_time);
+        $widget.html(html).addClass(school.status);
+        return;
       }
     }
+
+    $widget.html('Not found: ' + config.name).addClass('error-not-found');
   });
 });
